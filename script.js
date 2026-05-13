5ba05de582e7c8c362b4af2e4a1434490e8488c3
allaboutchisom <oduchino@gmail.com>
2026-05-13 01:01:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
-window.addEventListener("load", ()=>{
+function showMessage(){
+    document.getElementById("secretMessage").classList.toggle("hidden");
+}
 
-    const intro = document.getElementById("netflixIntro");
-    const main = document.getElementById("mainContent");
+const birthday = new Date("May 20, 2026 00:00:00").getTime();
 
-    // ensure main is ready but invisible
-    main.style.opacity = "0";
+setInterval(function(){
+
+    const now = new Date().getTime();
+
+    const distance = birthday - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+    days + "d " +
+    hours + "h " +
+    minutes + "m " +
+    seconds + "s ";
+
+},1000);
+
+function createHeart(){
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "💜";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+    heart.style.fontSize = Math.random() * 20 + 20 + "px";
+
+    document.body.appendChild(heart);
 
     setTimeout(()=>{
+        heart.remove();
+    },5000);
+}
 
-        // fade intro out
-        intro.style.opacity = "0";
-        intro.style.transition = "opacity 0.8s ease";
+setInterval(createHeart,300);
+const text = "You are not just special… you are unforgettable 💜";
+let i = 0;
 
-        // show main properly
-        main.style.opacity = "1";
+function typeWriter(){
+    if(i < text.length){
+        document.getElementById("typeText").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 80);
+    }
+}
 
-        main.style.transition = "opacity 0.8s ease";
-
-        setTimeout(()=>{
-            intro.style.display = "none";
-        },800);
-
-    },3000);
-
-});
+typeWriter();
 function openSong(){
 
     const song = document.getElementById("songPlayer");
